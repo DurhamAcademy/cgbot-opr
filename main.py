7eb126260d91c12eafb816f34049b85a038cf240
27moorev <124714046+27moorev@users.noreply.github.com>
2024-02-21 13:36:32 -0500

    did controllers and such

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def go_to_position(target_pos: tuple):
 def controller_mode():
     while True:
         left_speed, right_speed = nes.wpm_controller(nes.snes_input())
-        motor_driver.set_right_speed(left_speed)
+        motor_driver.set_left_speed(left_speed)
         motor_driver.set_right_speed(right_speed)
 
 try:
