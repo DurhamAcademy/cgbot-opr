e62791bc4be20e0d4f5c368db20aca2719e7ebfb
Forrest Beck <forrest.beck@da.org>
2024-02-27 09:38:59 -0500

    cleaned up some code

---
+++ stash_file.py
@@ -0,0 +1,49 @@
+
+def rotate_to_heading(current_heading, target_heading):
+    # Calculate difference between current and target heading
+    # Adjust the following formula based on your specific robot setup
+    heading_difference = (target_heading - current_heading + 360) % 360
+    # rotate right by default
+    rotation_dir = 1
+    if heading_difference >= 180:
+        # rotate left if that is shorter
+        rotation_dir = -1
+        print(rotation_dir)
+
+    current_heading = gps.get_heading()
+    while abs(target_heading - current_heading) > 5:
+        # rotate until real heading is close to target heading
+        print((abs(target_heading - current_heading) % 25) + 25)
+        motor_driver.set_left_speed(40 * rotation_dir)
+        motor_driver.set_right_speed(40 * rotation_dir * -1)
+        # update heading and rerun loop
+        current_heading = gps.get_heading()
+        print("cur", current_heading)
+        print("tar", target_heading)
+        print("goal", target_heading - current_heading)
+
+    """# Set motor speeds using PWM
+    motor_driver.set_left_speed(20)
+    motor_driver.set_right_speed(20)
+
+    # Move in a straight line for a specified duration
+    time.sleep(2)  # Adjust the duration as needed"""
+    print("finished")
+
+
+def go_to_position(target_pos: tuple):
+    current_pos = gps.get_gps_coords()
+    current_heading = gps.get_heading()
+    while gps.haversine_distance(current_pos, target_pos) > 1:
+        current_pos = gps.get_gps_coords()
+        current_heading = gps.get_heading()
+        target_heading = gps.calculate_heading(current_pos, target_pos)
+        rotate_to_heading(current_heading, target_heading)
+
+
+def controller_mode():
+    while True:
+        left_speed, right_speed = nes.wpm_controller(nes.snes_input())
+        motor_driver.set_left_speed(left_speed)
+        motor_driver.set_right_speed(right_speed)
+
