8cfd8a36b3b45ca74c4344a6fa7a190b1c8c859d
27moorev <124714046+27moorev@users.noreply.github.com>
2024-02-29 12:45:57 -0500

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -49,28 +49,28 @@ try:
     # rotate_to_heading(gps.get_heading(), gps.get_heading() + 90)
     # go_to_position((35.977222, -78.969176))
     while True:
-        if not controller.gps_mode:
+        left_speed, right_speed = controller.wpm_controller(controller.snes_input())
+        drive.set_left_speed(left_speed)
+        drive.set_right_speed(right_speed)
+        """if not controller.gps_mode:
             print("control")
             # If controller.gps_mode is False, then controller is enabled.
-            left_speed, right_speed = controller.wpm_controller(controller.snes_input())
-            drive.set_left_speed(left_speed)
-            drive.set_right_speed(right_speed)
+            
         elif controller.gps_mode:
-            print("gps")
-            # Else do cool GPS stuff
-            """    
-            pos = gps.get_gps_coords()
-            print("Position", pos)
-            pos = gps.get_gps_coords()
-            print("Position", pos)
-            heading = gps.get_heading()
-            print("Heading", heading)
-            speed = input()
-            motor_driver.set_right_speed(int(speed))
-            motor_driver.set_left_speed(int(speed))
-            current_heading = gps.get_heading()
-            rotate_to_heading(current_heading, (current_heading + -90) % 360)
-            print(gps.get_heading())
-            """
+            print("gps")"""
+        """    
+        pos = gps.get_gps_coords()
+        print("Position", pos)
+        pos = gps.get_gps_coords()
+        print("Position", pos)
+        heading = gps.get_heading()
+        print("Heading", heading)
+        speed = input()
+        motor_driver.set_right_speed(int(speed))
+        motor_driver.set_left_speed(int(speed))
+        current_heading = gps.get_heading()
+        rotate_to_heading(current_heading, (current_heading + -90) % 360)
+        print(gps.get_heading())
+        """
 finally:
     drive.cleanup()
