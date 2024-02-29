a28fae4a554571a56f764c74febd67b2a018a377
27moorev <124714046+27moorev@users.noreply.github.com>
2024-02-29 12:52:37 -0500

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def go_to_position(target_pos: tuple):
 
 try:
     # rotate_to_heading(gps.get_heading(), gps.get_heading() + 90)
-    go_to_position((35.97772, -78.96978))
+    go_to_position((35.97763, -78.96948))
     while False:
         left_speed, right_speed = controller.wpm_controller(controller.snes_input())
         drive.set_left_speed(left_speed)
