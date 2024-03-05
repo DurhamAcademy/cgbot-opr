112287945e952fc9990b1ac464c30265a0ffff6b
27moorev <124714046+27moorev@users.noreply.github.com>
2024-03-05 13:42:00 -0500

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def go_to_position(target_pos: tuple):
 
 try:
     # rotate_to_heading(gps.get_heading(), gps.get_heading() + 90)
-    # go_to_position((35.977669299999995, -78.9698552))
+    go_to_position((35.9776465, -78.96987879999999))
     print()
     while True:
         heading, coords = gps.get_gps_coords()
