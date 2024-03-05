f3276b00687b898f3f49367b477d7a8e6344ea6b
27moorev <124714046+27moorev@users.noreply.github.com>
2024-03-05 13:03:50 -0500

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ try:
         coords = gps.get_gps_coords()
         print(coords)
         print(gps.haversine_distance(gps.get_gps_coords(), (35.9776465, -78.96987879999999)))
-        print("hed", gps.calculate_heading(coords, (35.9776465, -78.96987879999999)))
+        print("head", gps.calculate_heading(coords, (35.9776465, -78.96987879999999)))
         """left_speed, right_speed = controller.wpm_controller(controller.snes_input())
         drive.set_left_speed(left_speed)
         drive.set_right_speed(right_speed)"""
