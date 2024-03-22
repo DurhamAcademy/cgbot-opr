39db2f1e008262c694caec3bc310396c67efc807
27moorev <124714046+27moorev@users.noreply.github.com>
2024-03-22 13:07:05 -0400

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ def main():
                 left_speed, right_speed = controller.wpm_controller(controller.snes_input())
                 drive.set_left_speed(left_speed)
                 drive.set_right_speed(right_speed)
+                print(gps.get_gps_coords())
 
                 """
                 print GPS head every second
