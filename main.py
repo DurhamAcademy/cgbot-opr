1fadf5c3db27cf7c93f792e594aef74270aaa013
27moorev <124714046+27moorev@users.noreply.github.com>
2024-03-26 12:51:42 -0400

    (justin committed this) removes nes prints

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def rotate_to_heading(current_heading, target_heading):
         # Would need to modify turn function to take in -35 to turn left
         if rotation_dir[0] == "left":
             # What is the destination degrees on the compass in relation to target_heading? / subtract for left turn
-            dest_compass = (current_compass - rotation_dir[1]) % 360
+            dest_compass = ((current_compass - rotation_dir[1]) - 180) % 360
             # speed = num_to_range(rotation_dir[1], 0, 360, 30, 50)
             while not within_range_degrees(current_compass, dest_compass):
                 drive.drive_turn_left(35)
@@ -145,7 +145,7 @@ def go_to_position(target_pos: tuple):
         current_pos = gps.get_gps_coords()
         current_heading = gps.gps_heading()
         # Use heading from GPS to determine a target_heading to destination coordinates
-        target_heading = (180 - gps.calculate_initial_compass_bearing(current_pos, target_pos)) % 360
+        target_heading = gps.calculate_initial_compass_bearing(current_pos, target_pos)
         print("targetheading: " + str(target_heading))
         rotate_to_heading(current_heading, target_heading)
         drive.drive_forward()
