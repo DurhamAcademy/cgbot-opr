f609692b8e6c2f082ac27c3aecd39f71bd0d8fe2
Forrest Beck <forrest.beck@da.org>
2024-04-26 09:29:04 -0400

    arduino_code

--- arduino.py
+++ arduino.py
@@ -5,46 +5,47 @@ import time
 class Arduino:
 
     def __init__(self):
-        self.ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_851393033313512102C2-if00', 9600, timeout=1)
+        self.ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_851393033313512102C2-if00',
+                                 9600,
+                                 timeout=1
+                                 )
+        self.ser.reset_input_buffer()
         self.ultrasonic_last_check = 0.00
+
+    def read_data(self, val):
+        self.ser.reset_input_buffer()
+        data = self.ser.readline()
+        d = data.decode()
+        d.split("|")
+        if val == "ultrasonic":
+            return d[0:3]
+        else:
+            return d[int(val)]
 
     def get_temperature(self):
         """
         Reads the temperature from the Arduino. Returns the temperature in degrees Celsius.
         :return: float degrees
         """
-        data = self.ser.readline()
-        d = data.decode()
-        d.split("|")
-        return float(d[4])
+        return float(self.read_data(4))
 
     def get_humidity(self):
         """
         Reads the humidity from the Arduino. Returns the humidity as float.
         :return: float humidity
         """
-        data = self.ser.readline()
-        d = data.decode()
-        d.split("|")
-        return float(d[5])
+        return float(self.read_data(5))
 
     def get_voltage(self):
         """
         Reads the voltage from Battery through the Arduino. Returns the voltage as float.
         :return: float voltage
         """
-        data = self.ser.readline()
-        d = data.decode()
-        d.split("|")
-        return float(d[6])
+        return float(self.read_data(6))
 
     def get_ultrasonic(self):
         """
         Reads the ultrasonic sensors from the Arduino. Returns the ultrasonic as
-        :return: dictionary
+        :return: list
         """
-        data = self.ser.readline()
-        d = data.decode()
-        d.split("|")
-        self.ultrasonic_last_check = time.time()
-        return d[0:3]
+        return self.read_data("ultrasonic")
